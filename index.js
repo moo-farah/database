ed45224745fd92e77b27c34b8574613730fedb1f
moo-farah <rizmik.farah@gmail.com>
2026-08-26 16:48:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import { PGlite } from "@electric-sql/pglite";
+import fs from 'fs'
+
+(async () => {
+    const db = new PGlite();
+    await db.exec (`
+        CREATE TABLE IF NOT EXISTS cars (
+            id SERIAL PRIMARY KEY,
+            brand TEXT,
+            model TEXT,
+            year INTEGER,
+            price INTEGER,
+            color TEXT,
+            condition INTEGER,
+            solid BOOLEAN
+        );
+        INSERT INTO cars (brand, model, year, price, color, condition, solid
+            ) VALUES 
+            ('Toyota', 'Camry', 2020, 25000, 'blue', 8, true),
+            ('Honda', 'Civic', 2019, 20000, 'red', 7, false),
+            ('Ford', 'F-150', 2021, 35000, 'black', 9, true),
+            ('Chevrolet', 'Silverado', 2020, 30000, 'white', 8, true),
+            ('BMW', 'X5', 2019, 50000, 'green', 7, false)
+    `);
+    // Load the SQL file
+    const query = fs.readFileSync('query.sql', 'utf-8');
+
+    // Executing simple queries for sections
+    const response = await db.query(query);
+
+    console.clear();
+    console.table(response.rows, ['brand', 'model', 'year', 'price', 'color', 'condition', 'solid']);
+})();
---
+++ notes.md
@@ -0,0 +1,18 @@
+# What is Structured Query Language (SQL)
+
+Is the standard language used to communicate with relational databases - systems that store data in tables made up of rows and columns.
+columns: known as **fields** while rows known as **records**
+
+
+
+**What is you use it for:**
+
+- **Querying data:**  retrieving specific information from tables(**SELECT**)
+- **Inserting data:** adding new rows (**INSERT**)
+- **Updating data:**  modifying existing rows (**UPDATE**)
+- **Deleting data:**  removing rows (**DELETE**)
+- **Defining structure:** creating/altering tables, setting relationships, constraints, etc. (**CREATE, ALTER, DROP**)
+
+**Object Relational Mapping (ORMS)**
+Is a method of interacting with databases which allows use of an object-oriented approach, rather than using SQL direct.
+
